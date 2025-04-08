fix metrics computation and tensorboard logging in evaluator class

src/evaluator.py
@@ -1,7 +1,7 @@
 import json
 import pathlib
 import numpy as np
-from sklearn.metrics import confusion_matrix,  accuracy_score, precision_score, recall_score
+import sklearn
 import matplotlib.pyplot as plt
 import seaborn as sns
 from typing import Dict, Any, Union
@@ -12,6 +12,7 @@ class Evaluator:
     def __init__(self, labelmap_path: Union[str, pathlib.Path], log_dir: str, threshold: int=0.5):
         self.labelmap = self._load_labelmap(labelmap_path)
         self.class_names = [self.labelmap[i] for i in sorted(self.labelmap.keys())]
+        self.class_indices = sorted(self.labelmap.keys())
         self.log_dir = pathlib.Path(log_dir)
         self.log_dir.mkdir(parents=True, exist_ok=True)
         self.threshold = threshold
@@ -22,33 +23,49 @@ class Evaluator:
             labelmap = json.load(f)
         return {int(k): v for k, v in labelmap.items()}
 
-    def compute_metrics(self, y_true: np.ndarray, y_pred: np.ndarray) -> Dict[str, Any]:
-        
-        y_pred_top1 = y_pred.argmax(axis=1)
-        acc = accuracy_score(y_true, y_pred_top1)
-        cm = confusion_matrix(y_true, y_pred_top1, normalize='true')
-        global_precision = precision_score(y_true, y_pred)
-        global_recall = recall_score(y_true, y_pred)
-        
-        per_class_metrics = {}
+    def compute_precision(self, y_true, y_pred):
+        TP = {}
+        FP = {}
+        precision = {}
         for class_idx, class_name in self.labelmap.items():
-            y_true_class = (y_true == class_idx).astype(int)
-            y_pred_class = (y_pred_top1 == class_idx).astype(int)
-            per_class_metrics[class_name] = {
-                "accuracy": accuracy_score(y_true_class, y_pred_class),
-                "precision": precision_score(y_true_class, y_pred_class, zero_division=0),
-                "recall": recall_score(y_true_class, y_pred_class, zero_division=0)
-            }
+            TP[class_name] = np.sum((y_pred == class_idx)*(y_true == class_idx))
+            FP[class_name] = np.sum((y_pred == class_idx)*(y_true != class_idx))
+            precision[class_name] = TP[class_name]/(TP[class_name]+FP[class_name]+1e-7)
+        precision["mean"] = np.mean(list(precision.values()))
+        return precision
 
-        return {
-            "global": {"accuracy": acc, "precision": global_precision, "recall": global_recall, "confusion_matrix": cm},
-            "per_class": per_class_metrics
-        }
+    def compute_recall(self, y_true, y_pred):
+        TP={}
+        FN={}
+        recall={}
+        for class_idx, class_name in self.labelmap.items():
+            TP[class_name] = np.sum((y_pred==class_idx)*(y_true==class_idx))
+            FN[class_name]=np.sum((y_pred!=class_idx)*(y_pred==class_idx))
+            recall[class_name] = TP[class_name]/(TP[class_name]+FN[class_name]+1e-7)
+        recall["mean"] = np.mean(list(recall.values()))
+        return recall
+    
+    def compute_accuracy(self, y_true, y_pred):
+        accuracy={}
+        for class_idx, class_name in self.labelmap.items():
+            accuracy[class_name] = np.sum((y_pred==class_idx)*(y_true==class_idx))/(np.sum(y_true==class_idx)+1e-7)
+        accuracy["mean"] = np.mean(list(accuracy.values()))
+        return accuracy
+
+    def compute_metrics(self, y_true: np.ndarray, y_pred: np.ndarray) -> Dict:    
+        y_pred_top1 = y_pred.argmax(axis=-1)
+        cm = sklearn.metrics.confusion_matrix(y_true, y_pred_top1, normalize='true')
+        accuracy = self.compute_accuracy(y_true, y_pred_top1)
+        precision = self.compute_precision(y_true, y_pred_top1)
+        recall = self.compute_recall(y_true, y_pred_top1)
+
+        return {"accuracy": accuracy, "precision": precision, "recall": recall, "confusion_matrix": cm}
+ 
 
     def save_results(self, metrics: Dict[str, Any]) -> None:
         with open(os.path.join(self.log_dir,"metrics.json"), "w") as f:
             json.dump(metrics, f, indent=2)
-        self._plot_confusion_matrix(np.array(metrics["global"]["confusion_matrix"]),save_path=os.path.join(self.log_dir, "confusion_matrix.png"))
+        self._plot_confusion_matrix(np.array(metrics["confusion_matrix"]),save_path=os.path.join(self.log_dir, "confusion_matrix.png"))
 
     
     def _plot_confusion_matrix(self, cm: np.ndarray, save_path: pathlib.Path) -> None:
@@ -62,31 +79,18 @@ class Evaluator:
 
     def _create_writers(self):
         tb_writers = {}
-        for class_name in self.labelmap.values():
-            folder_path = os.path.join(self.log_dir, class_name)
+        for item in list(self.labelmap.values())+["mean"]:
+            folder_path = os.path.join(self.log_dir, item)
             if not os.path.exists(folder_path): os.makedirs(folder_path)
-            tb_writers[class_name] = tf.summary.create_file_writer(folder_path)
-        
-        folder_path = os.path.join(self.log_dir, "global")
-        if not os.path.exists(folder_path): os.makedirs(folder_path)
-        tb_writers["global"] = tf.summary.create_file_writer(folder_path)
+            tb_writers[item] = tf.summary.create_file_writer(folder_path)
         return tb_writers
 
-    def log_to_tensorboard(self, metrics, epoch, name):
-        with self.tb_writers['global']['accuracy'].as_default():
-            tf.summary.scalar(name=f"{name}/global_accuracy", data=metrics["global"]["accuracy"], step=epoch)
-        with self.tb_writers['global']['precision'].as_default():
-            tf.summary.scalar(name=f"{name}/global_precision", data=metrics["global"]["precision"], step=epoch)
-        with self.tb_writers['global']['recall'].as_default():
-            tf.summary.scalar(name=f"{name}/global_recall", data=metrics["global"]["recall"], step=epoch)
-
-        for class_name in self.evaluator.labelmap.values():
-            with self.tb_writers["per_class"][class_name]["accuracy"].as_default():
-                tf.summary.scalar(name=f"{name}/accuracy", data=metrics["per_class"][class_name]["accuracy"], step=epoch)
-            with self.tb_writers["per_class"][class_name]["precision"].as_default():
-                tf.summary.scalar(name=f"{name}/precision", data=metrics["per_class"][class_name]["precision"], step=epoch)
-            with self.tb_writers["per_class"][class_name]["recall"].as_default():
-                tf.summary.scalar(name=f"{name}/recall", data=metrics["per_class"][class_name]["recall"], step=epoch)
+    def log_to_tensorboard(self, metrics, epoch, name):    
+        for item in list(self.labelmap.values())+["mean"]:
+            with self.tb_writers[item].as_default():
+                tf.summary.scalar(name=f"{name}/accuracy", data=metrics["accuracy"][item], step=epoch)
+                tf.summary.scalar(name=f"{name}/precision", data=metrics["precision"][item], step=epoch)
+                tf.summary.scalar(name=f"{name}/recall", data=metrics["recall"][item], step=epoch)
 
 
 class EvaluationCallback(tf.keras.callbacks.Callback):